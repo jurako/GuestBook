fc5f5c4715dfad01a06e6d9b940ebcc9488aaf73
jurako <jurako91@gmail.com>
2016-08-27 18:08:48 +0300

    27.08.16. work

--- saveMessage.php
+++ saveMessage.php
@@ -1,23 +1,13 @@
 <?php
-    require_once '/components/db_connection.php';
-    require_once '/components/functions.php';
-    
-    //user input validation
-    $validationErrors = false;
-    
-    if (checkUsername($_POST['username'])) {
-        echo 'all good';
-    } else 
-        $errors[] = '' 
-    
-    
-    // get browser info
-    $ua=getBrowser();
-    $user_browser= $ua['name'] . " " . $ua['version'];   
+    function saveMessage() {
+        // get browser info
+        $ua=getBrowser();
+        $user_browser= $ua['name'] . " " . $ua['version'];   
 
-    $pdoStatement = $db->prepare("INSERT INTO messages (username, email, text, homepage, user_ip, user_agent) "
-                                . "VALUES (:username, :email, :text, :homepage, :user_ip, :user_agent)");
-    $pdoStatement->execute(array(':username' => $_POST['username'], ':email' => $_POST['email'],
-                                 ':text' => $_POST['text'], ':homepage' => $_POST['homepage'],
-                                 ':user_ip' => $_SERVER['REMOTE_ADDR'], ':user_agent' => $user_browser));
+        $pdoStatement = $db->prepare("INSERT INTO messages (username, email, text, homepage, user_ip, user_agent) "
+                                     . "VALUES (:username, :email, :text, :homepage, :user_ip, :user_agent)");
+        $pdoStatement->execute(array(':username' => $_POST['username'], ':email' => $_POST['email'],
+                                      ':text' => $_POST['text'], ':homepage' => $_POST['homepage'],
+                                      ':user_ip' => $_SERVER['REMOTE_ADDR'], ':user_agent' => $user_browser));
+    }
 ?>
